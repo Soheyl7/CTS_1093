1d73954dc4f9772bb0e597ef4684196289511e0d
Mohebbi Soheyl <mohebbisoheyl@yahoo.com>
2021-04-15 15:21:30 +0300

    a

--- week 8/src/ro/ase/csie/cts/g1093/dp/decorator/ArmorDecorator.java
+++ week 8/src/ro/ase/csie/cts/g1093/dp/decorator/ArmorDecorator.java
@@ -1,5 +1,24 @@
 package ro.ase.csie.cts.g1093.dp.decorator;
 
-public class ArmorDecorator {
+import ro.ase.csie.cts.g1093.dp.adapter.ACMECharacter;
 
+public class ArmorDecorator extends ACMEAbstractDecorator {
+	
+	int armorLevel;
+
+	public ArmorDecorator(ACMECharacter hero, int armorLevel) {
+		super(hero);
+		this.armorLevel= armorLevel;
+		
+	}
+
+	@Override
+	public void takeAHit(int points) {
+		int lostPoints = points - armorLevel;
+		if(lostPoints > 0)
+			this.character.takeAHit(lostPoints);
+		
+	}
+
+	
 }
